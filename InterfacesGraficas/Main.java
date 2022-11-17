2d95a32a5c18f3d9a2c2563b4b24ef78f327358c
OhMariaaaz <mgomes741741@gmail.com>
2022-11-17 07:29:26 -0300

    Dando os primeiros passos em interface gráfica no Java. Um pouco de swing e JFrame.

---
+++ InterfacesGraficas/Main.java
@@ -0,0 +1,25 @@
+package InterfacesGraficas;
+
+public class Main{
+    public static void main(String[] args){
+
+        /*
+        JFrame frame = new JFrame();
+        frame.setTitle("Primeiro programinha!");
+        frame.setSize(500,500);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon imagem = new ImageIcon("icon.jpg");
+
+        frame.setIconImage(imagem.getImage());
+        frame.getContentPane().setBackground(new Color(0xFFFFFF));
+
+        //adicione as paradas aqui
+        //frame.pack();
+        */
+
+    }
+
+}
---
+++ InterfacesGraficas/PrimeiraTela.java
@@ -0,0 +1,7 @@
+package InterfacesGraficas;
+import java.awt.*;
+import javax.swing.*;
+
+public class PrimeiraTela extends JFrame{
+    
+}
